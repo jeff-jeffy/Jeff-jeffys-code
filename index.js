597f422bc9e2246b20d75fe45c2fa585559c1f3c
jeff-jeffy <156810817+jeff-jeffy@users.noreply.github.com>
2024-01-16 17:51:33 +0400

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["👑 I am RTX","Spotify","Youtube","SUBSCRIBE❤️","THANKS 🌇"];
+const statusMessages = ["HALLO IZ ME, FIE"];
 
 
 let currentIndex = 0;
